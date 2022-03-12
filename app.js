d40ec2ea746d01ee1a32f721eaa6f1291b8026be
sabahya3 <sabahya3@gmail.com>
2022-03-12 17:27:42 +0200

    baby

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 
 // Routes Middle Ware
 
-app.use('/products', myRouter);
+app.use('/', myRouter);
 
 mongoose.connect('mongodb+srv://ahmed:dTIm8PLHB133XpP6@cluster0.4lxon.mongodb.net/myDb?retryWrites=true&w=majority' , ()=>{
 console.log('connected to mongo succefully');
--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ const Product = require('../models/product');
 
 
 
-router.get('/', async (req, res) => {
+router.get('/products', async (req, res) => {
     try {
         const product = await Product.find();
 
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 });
 
 // get element by id 
-router.get('/:id', async (req, res) => {
+router.get('/products/:id', async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
 
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
 
 
 // delete element by id 
-router.patch('/:id', async (req, res) => {
+router.patch('/products/:id', async (req, res) => {
     try {
         const product = await Product.updateOne({ "_id": req.params.id }, { "title": req.body.title });
 
@@ -44,7 +44,7 @@ router.patch('/:id', async (req, res) => {
 
 
 // delete element by id 
-router.delete('/:id', async (req, res) => {
+router.delete('/products/:id', async (req, res) => {
     try {
         const product = await Product.deleteOne({ "_id": req.params.id });
 
